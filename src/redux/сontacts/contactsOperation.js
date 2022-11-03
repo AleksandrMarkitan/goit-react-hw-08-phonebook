de6a23f9eedd3e48753b434815c599cf46b445ad
AleksandrMarkitan <0688138@gmail.com>
2022-11-03 02:28:54 +0200

    start

---
+++ src/redux/сontacts/contactsOperation.js
@@ -0,0 +1,40 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://635ab54f6f97ae73a6348c56.mockapi.io/hw07';
+
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  async (_, { rejectWithValue }) => {
+    try {
+      const { data } = await axios('/contacts');
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  'contacts/addContact',
+  async (contact, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post('/contacts', contact);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContact',
+  async (id, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.delete(`/contacts/${id}`);
+      return data.id;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
